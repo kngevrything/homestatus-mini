cbf750655e85fc718f3f334414507114de171157
Kevin Liedtke <kngevrything@gmail.com>
2026-05-10 00:14:33 -0700

    Add Wi-Fi config template

---
+++ firmware/homestatus-mini/wifi_config.example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Copy this file to wifi_config.h and fill in your local credentials.
+// Do not commit wifi_config.h.
+
+const char* WIFI_SSID = "your-wifi-name";
+const char* WIFI_PASSWORD = "your-wifi-password";
